6ab79a22a16203ec45ba3ce575ea2ebdbf530825
cdans <ch.dansard@gmail.com>
2023-09-06 00:01:09 +0200

    fix nav

--- src/features/app/index.tsx
+++ src/features/app/index.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Layout } from "./Layout";
 import { Box, Typography } from "@mui/material";
-import { ArticleOverviewPage, ArticlePage, ArticleResultsPage } from "@features/article";
+import { ArticleOverviewPage, ArticleResultsPage } from "@features/article";
 
 const NoMatch = () => {
   return (
@@ -23,7 +23,6 @@ export const App = () => {
           <Route element={<Layout />}>
             <Route index element={<ArticleOverviewPage />} />
             <Route path="article-results" element={<ArticleResultsPage />} />
-            <Route path="article/:id" element={<ArticlePage />} />
             <Route path="*" element={<NoMatch />} />
           </Route>
         </Routes>
